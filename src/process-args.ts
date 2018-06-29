fbd72445e2df0d4c6523403a930bbdbcdbd0cc14
Tanner Goins <TGoins@IWTHealth.com>
2018-06-29 13:58:17 -0500

    prevent option values from being parsed as columns

--- src/process-args.ts
+++ src/process-args.ts
@@ -2,16 +2,23 @@ import { OptionName, options } from './options'
 
 export const processArgs = (args: string[]) => {
   const uniqueColumns: string[] = []
+  let skip = false
   args.forEach((arg, i) => {
+    if (skip) {
+      skip = false
+      return
+    }
     const argFiltered = arg.removeAll('-')
     if (isOption(argFiltered)) {
       if (arg.startsWith('--')) {
         if (args[i + 1] && isBool(args[i + 1])) {
           options[argFiltered].value = Boolean(args[i + 1])
+          skip = true
           return
         }
         if (Number(args[i + 1])) {
           options[argFiltered].value = Number(args[i + 1])
+          skip = true
           return
         }
       }
